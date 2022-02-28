3ba515d5be3511bfea567445da82262121b8251a
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-02-28 14:22:31 +0600

    last upload

--- javascript/dogAPI.js
+++ javascript/dogAPI.js
@@ -10,17 +10,18 @@ fetch(url)
 //display dog list......
 const displayDogs = (DogList) => {
  const DisplayData = document.getElementById("display_Data");
- const Display10Data = DogList.slice(0,10);
+ const Display100Data = DogList.slice(0,100);
   
- for(const Dog of Display10Data){
+ for(const Dog of Display100Data){
      const div = document.createElement ("div")
+     div.className = "col-lg-4 d-flex justify-content-center";
      div.innerHTML =`
-     <div class="card" style="width: 18rem; d-flex">
+     <div class="card mb-3" style="width: 18rem; ">
      <img src="${Dog.image.url}" class="card-img-top" alt="...">
      <div class="card-body">
        <h4 class="card-title">Name:${Dog.name}</h4>
        <p class="card-text">Temperament:${Dog.temperament}</p>
-       <a href="#" class="btn btn-primary">Go somewhere</a>
+       <a href="#" class="btn btn-primary">Learn More</a>
      </div>
    </div>
      `
